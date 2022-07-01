0a8f9050a84b309d2e5438c4bb61c122dbc2699e
Nelio <neliocarlos95@gmail.com>
2022-07-01 15:23:53 -0400

    Implementado Exception no FuncionarioController

--- AtacadoApi/Controllers/FuncionarioController.cs
+++ AtacadoApi/Controllers/FuncionarioController.cs
@@ -25,8 +25,15 @@ namespace AtacadoApi.Controllers
         [HttpGet("{skip:int}/{take:int}")]
         public ActionResult<List<FuncionarioPoco>> GetPage(int skip, int take)
         {
-            List<FuncionarioPoco> listaResposta = this.servico.Listar(skip, take);
-            return Ok(listaResposta);
+            try
+            {
+                List<FuncionarioPoco> listaResposta = this.servico.Listar(skip, take);
+                return Ok(listaResposta);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }           
         }
 
         /// <summary>
@@ -37,8 +44,16 @@ namespace AtacadoApi.Controllers
         [HttpGet("{id:int}")]
         public ActionResult<FuncionarioPoco> GetById(int id)
         {
-            FuncionarioPoco pocoResposta = this.servico.Selecionar(id);
-            return Ok(pocoResposta);
+            try
+            {
+                FuncionarioPoco pocoResposta = this.servico.Selecionar(id);
+                return Ok(pocoResposta);
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
         }
 
         /// <summary>
@@ -49,8 +64,16 @@ namespace AtacadoApi.Controllers
         [HttpGet("matricula/{mat:long}")]
         public ActionResult<FuncionarioPoco> GetPorMatricula(long mat)
         {
-            FuncionarioPoco pocoResposta = this.servico.SelecionarPorMatricula(mat);
-            return Ok(pocoResposta);
+            try
+            {
+                FuncionarioPoco pocoResposta = this.servico.SelecionarPorMatricula(mat);
+                return Ok(pocoResposta);
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
         }
 
         /// <summary>
@@ -61,13 +84,21 @@ namespace AtacadoApi.Controllers
         [HttpPost]
         public ActionResult<FuncionarioPoco> Post([FromBody] FuncionarioPoco poco)
         {
-            FuncionarioPoco pocoResposta = this.servico.Criar(poco);
-            if (pocoResposta == null)
+            try
             {
-                return BadRequest(this.servico.MensagensProcess);
+                FuncionarioPoco pocoResposta = this.servico.Criar(poco);
+                if (pocoResposta == null)
+                {
+                    return BadRequest(this.servico.MensagensProcess);
+                }
+                else
+                    return Ok(pocoResposta);
             }
-            else
-                return Ok(pocoResposta);
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
         }
 
         /// <summary>
@@ -78,8 +109,16 @@ namespace AtacadoApi.Controllers
         [HttpPut]
         public ActionResult<FuncionarioPoco> Put([FromBody] FuncionarioPoco poco)
         {
-            FuncionarioPoco pocoResposta = this.servico.Atualizar(poco);
-            return Ok(pocoResposta);
+            try
+            {
+                FuncionarioPoco pocoResposta = this.servico.Atualizar(poco);
+                return Ok(pocoResposta);
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
         }
 
         /// <summary>
@@ -90,8 +129,16 @@ namespace AtacadoApi.Controllers
         [HttpDelete]
         public ActionResult<FuncionarioPoco> Delete([FromBody] FuncionarioPoco poco)
         {
-            FuncionarioPoco pocoResposta = this.servico.Excluir(poco);
-            return Ok(pocoResposta);
+            try
+            {
+                FuncionarioPoco pocoResposta = this.servico.Excluir(poco);
+                return Ok(pocoResposta);
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
         }
 
         /// <summary>
@@ -102,8 +149,16 @@ namespace AtacadoApi.Controllers
         [HttpDelete("{id:int}")]
         public ActionResult<FuncionarioPoco> Delete(int id)
         {
-            FuncionarioPoco pocoResposta = this.servico.Excluir(id);
-            return Ok(pocoResposta);
+            try
+            {
+                FuncionarioPoco pocoResposta = this.servico.Excluir(id);
+                return Ok(pocoResposta);
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(ex.Message);
+            }
         }
     }
 }
